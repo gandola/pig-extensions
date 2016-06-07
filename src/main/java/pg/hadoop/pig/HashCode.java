138cd38143179aac7b26d18a08d31ccbddf4f7f2
gandola <pedro@divvit.com>
2016-06-07 19:21:18 +0100

    Add pig HashCode UDF.

---
+++ src/main/java/pg/hadoop/pig/HashCode.java
@@ -0,0 +1,25 @@
+package pg.hadoop.pig;
+
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+
+import java.io.IOException;
+
+/**
+ * Returns the hashcode of the given String or Number.
+ */
+public class HashCode extends EvalFunc<Integer> {
+
+    @Override
+    public Integer exec(final Tuple input) throws IOException {
+        if (input.size() != 1) {
+            throw new IllegalArgumentException("HashCode function requires a String or Number parameter.");
+        }
+
+        final Object obj = input.get(0);
+        if (!(obj instanceof String) && !(obj instanceof Number)) {
+            throw new IllegalArgumentException("HashCode only supports Strings and Numbers");
+        }
+        return obj.hashCode();
+    }
+}
